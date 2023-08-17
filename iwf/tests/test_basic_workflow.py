1e3b324d805183c4923c68796344f7d65607e673
Quanzheng Long <qlong@indeed.com>
2023-08-17 15:13:08 -0700

    Revert "remove flask"
    This reverts commit f5acf74e7e56223f504f80b959c19f6404435811.

--- iwf/tests/test_basic_workflow.py
+++ iwf/tests/test_basic_workflow.py
@@ -1,8 +1,5 @@
 import inspect
 import time
-import unittest
-
-from flask import Flask
 
 from iwf.client import Client
 from iwf.registry import Registry
@@ -21,17 +18,3 @@ def test_start_workflow():
     wf_id = f"{inspect.currentframe().f_code.co_name}-{time.time_ns()}"
 
     client.start_workflow(HelloWorkflow, wf_id, 1)
-
-
-class TestHelloWorkflow(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.app = Flask(__name__)
-        app.run()
-
-    def test_length_of_list(self):
-        self.assertEqual(len(self.test_list), 5)
-
-    @classmethod
-    def tearDownClass(cls):
-        del cls.app
