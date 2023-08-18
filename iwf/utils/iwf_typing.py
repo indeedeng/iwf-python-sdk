1785b1aca5296e3f8ed0b75b4b4e4ce6385fc4a9
Quanzheng Long <qlong@indeed.com>
2023-08-18 09:48:08 -0700

    fix nontype

--- iwf/utils/iwf_typing.py
+++ iwf/utils/iwf_typing.py
@@ -5,16 +5,7 @@ from iwf_api.types import Unset
 T = TypeVar("T")
 
 
-def none_type() -> type[T]:
-    """
-    This returns the NoneType to make it more friendly to use.
-    Alternatively, users can also use Python builtin types.NoneType but mypy will complain about it.
-
-    NOTE: this only works for Python 3.9 and above
-    Returns:
-        The type for None
-    """
-    return type(None)  # type: ignore
+NoneType: type = type(None)
 
 
 def check_not_unset(input: Union[Unset, T]) -> T:
