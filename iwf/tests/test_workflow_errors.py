dfd05463de48689db0c64dcd3f06172bd87198b9
Quanzheng Long <qlong@indeed.com>
2023-09-09 13:51:31 -0700

    420 errors

---
+++ iwf/tests/test_workflow_errors.py
@@ -0,0 +1,108 @@
+import inspect
+import time
+import unittest
+
+from iwf_api.models import WorkflowStopType
+
+from iwf.client import Client
+from iwf.command_request import CommandRequest, InternalChannelCommand
+from iwf.command_results import CommandResults
+from iwf.communication import Communication
+from iwf.errors import (
+    WorkflowCanceled,
+    WorkflowFailed,
+    WorkflowStillRunningError,
+    WorkflowTerminated,
+    WorkflowTimeout,
+)
+from iwf.persistence import Persistence
+from iwf.state_decision import StateDecision
+from iwf.state_schema import StateSchema
+from iwf.stop_workflow_options import StopWorkflowOptions
+from iwf.tests.worker_server import registry
+from iwf.workflow import ObjectWorkflow
+from iwf.workflow_context import WorkflowContext
+from iwf.workflow_state import T, WorkflowState
+
+test_channel_name = "test-name"
+
+
+class WaitState(WorkflowState[None]):
+    def wait_until(
+        self,
+        ctx: WorkflowContext,
+        input: T,
+        persistence: Persistence,
+        communication: Communication,
+    ) -> CommandRequest:
+        return CommandRequest.for_all_command_completed(
+            InternalChannelCommand.by_name(test_channel_name)
+        )
+
+    def execute(
+        self,
+        ctx: WorkflowContext,
+        input: T,
+        command_results: CommandResults,
+        persistence: Persistence,
+        communication: Communication,
+    ) -> StateDecision:
+        return StateDecision.graceful_complete_workflow()
+
+
+class WaitWorkflow(ObjectWorkflow):
+    def get_workflow_states(self) -> StateSchema:
+        return StateSchema.with_starting_state(WaitState())
+
+
+wf = WaitWorkflow()
+registry.add_workflow(wf)
+client = Client(registry)
+
+
+class TestWorkflowErrors(unittest.TestCase):
+    def test_workflow_timeout(self):
+        wf_id = f"{inspect.currentframe().f_code.co_name}-{time.time_ns()}"
+        client.start_workflow(WaitWorkflow, wf_id, 1)
+        with self.assertRaises(WorkflowTimeout):
+            client.get_simple_workflow_result_with_wait(wf_id, str)
+
+    def test_workflow_still_running_when_wait(self):
+        wf_id = f"{inspect.currentframe().f_code.co_name}-{time.time_ns()}"
+        # client_options = ClientOptions.local_default()
+        # client_options.api_timeout = 5
+        # TODO using a shorter api timeout will throw a different timeout eror, it's better to unify it
+        client.start_workflow(WaitWorkflow, wf_id, 61)
+        with self.assertRaises(WorkflowStillRunningError):
+            client.get_simple_workflow_result_with_wait(wf_id, str)
+
+    def test_workflow_canceled(self):
+        wf_id = f"{inspect.currentframe().f_code.co_name}-{time.time_ns()}"
+        client.start_workflow(WaitWorkflow, wf_id, 10)
+        client.stop_workflow(wf_id)
+        with self.assertRaises(WorkflowCanceled):
+            client.get_simple_workflow_result_with_wait(wf_id, str)
+
+    def test_workflow_terminated(self):
+        wf_id = f"{inspect.currentframe().f_code.co_name}-{time.time_ns()}"
+        client.start_workflow(WaitWorkflow, wf_id, 10)
+        client.stop_workflow(
+            wf_id,
+            StopWorkflowOptions(
+                workflow_stop_type=WorkflowStopType.TERMINATE, reason="test"
+            ),
+        )
+        with self.assertRaises(WorkflowTerminated):
+            client.get_simple_workflow_result_with_wait(wf_id, str)
+
+    def test_workflow_failed(self):
+        wf_id = f"{inspect.currentframe().f_code.co_name}-{time.time_ns()}"
+        client.start_workflow(WaitWorkflow, wf_id, 10)
+        client.stop_workflow(
+            wf_id,
+            StopWorkflowOptions(
+                workflow_stop_type=WorkflowStopType.FAIL, reason="test"
+            ),
+        )
+        with self.assertRaises(WorkflowFailed):
+            client.get_simple_workflow_result_with_wait(wf_id, str)
--- iwf/unregistered_client.py
+++ iwf/unregistered_client.py
@@ -1,5 +1,6 @@
 import http
 from dataclasses import dataclass
+from http import HTTPStatus
 from typing import Any, List, Optional, Type, TypeVar
 
 from iwf_api import Client
@@ -64,10 +65,36 @@ class UnregisteredWorkflowOptions:
 T = TypeVar("T")
 
 
+# from https://stackoverflow.com/questions/45028991/best-way-to-extend-httpstatus-with-custom-value
+# HERE BE DRAGONS!
+# DO NOT do this unless you absolutely have to.
+def add_http_status(name, value, phrase, description=""):
+    # call our new member factory, it's essentially the `HTTPStatus.__new__` method
+    new_status = HTTPStatus.__new_member__(HTTPStatus, value, phrase, description)
+    new_status._name_ = name  # store the enum's member internal name
+    new_status.__objclass__ = (
+        HTTPStatus.__class__
+    )  # store the enum's member parent class
+    setattr(HTTPStatus, name, new_status)  # add it to the global HTTPStatus namespace
+    HTTPStatus._member_map_[name] = new_status  #  add it to the name=>member map
+    HTTPStatus._member_names_.append(
+        name
+    )  # append the names so it appears in __members__
+    HTTPStatus._value2member_map_[value] = new_status  # add it to the value=>member map
+
+
+add_http_status("IWF_CUSTOM_ERROR_1", 420, "IWF_CUSTOM_ERROR_1")
+add_http_status("IWF_CUSTOM_ERROR_2", 450, "IWF_CUSTOM_ERROR_2")
+
+
 class UnregisteredClient:
     def __init__(self, client_options: ClientOptions):
         self.client_options = client_options
-        self.api_client = Client(base_url=client_options.server_url, timeout=60)
+        self.api_client = Client(
+            base_url=client_options.server_url,
+            timeout=client_options.api_timeout,
+            raise_on_unexpected_status=True,
+        )
 
     def start_workflow(
         self,
