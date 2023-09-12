446bcc94e50e0a6f4c7b3e026da8d2722905303c
Quanzheng Long <qlong@indeed.com>
2023-09-12 09:54:14 -0700

    done all

--- iwf/command_results.py
+++ iwf/command_results.py
@@ -27,17 +27,27 @@ class InternalChannelCommandResult:
 
 
 @dataclass
+class SignalChannelCommandResult:
+    channel_name: str
+    value: Any
+    status: ChannelRequestStatus
+    command_id: str
+
+
+@dataclass
 class CommandResults:
     timer_commands: list[TimerCommandResult]
     internal_channel_commands: list[InternalChannelCommandResult]
+    signal_channel_commands: list[SignalChannelCommandResult]
 
 
 def from_idl_command_results(
     idl_results: Union[Unset, IdlCommandResults],
     internal_channel_types: dict[str, typing.Optional[type]],
+    signal_channel_types: dict[str, typing.Optional[type]],
     object_encoder: ObjectEncoder,
 ) -> CommandResults:
-    results = CommandResults(list(), list())
+    results = CommandResults(list(), list(), list())
     if isinstance(idl_results, Unset):
         return results
     if not isinstance(idl_results.timer_results, Unset):
@@ -56,6 +66,20 @@ def from_idl_command_results(
                     ),
                     inter.request_status,
                     inter.command_id,
+                )
+            )
+
+    if not isinstance(idl_results.signal_results, Unset):
+        for sig in idl_results.signal_results:
+            results.signal_channel_commands.append(
+                SignalChannelCommandResult(
+                    sig.signal_channel_name,
+                    object_encoder.decode(
+                        sig.signal_value,
+                        signal_channel_types.get(sig.signal_channel_name),
+                    ),
+                    sig.signal_request_status,
+                    sig.command_id,
                 )
             )
     return results
