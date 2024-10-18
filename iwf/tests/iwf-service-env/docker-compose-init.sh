d29c1d60a912a56bb0724633c81b72dac5fe8411
Samuel Caçador <samuelmoreira27@gmail.com>
2024-10-18 20:23:15 +0100

    Revert "IWF-206: Upgrade Temporal SDKs in iWF SDKs test envs"
    This reverts commit c868d54d929560eb201a17ff56eae65a6d2abc84.

--- iwf/tests/iwf-service-env/docker-compose-init.sh
+++ iwf/tests/iwf-service-env/docker-compose-init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# use new version of tctl so that we can skip the prompt
+tctl config set version next
+
+
 checkExists () {
   # see https://github.com/temporalio/temporal/issues/4160
-  if temporal  operator search-attribute list | grep -q "$1"; then
+  if tctl search-attribute list | grep -q "$1"; then
     return 0
   else
     return 1
@@ -12,31 +16,31 @@ fi
 echo "now trying to register iWF system search attributes..."
 for run in {1..120}; do
   sleep 1
-  temporal  operator search-attribute  create --name IwfWorkflowType --type Keyword
+  tctl search-attribute  create -name IwfWorkflowType -type Keyword -y
   sleep 0.1
-  temporal  operator search-attribute  create --name IwfGlobalWorkflowVersion --type Int
+  tctl search-attribute  create -name IwfGlobalWorkflowVersion -type Int -y
   sleep 0.1
-  temporal  operator search-attribute  create --name IwfExecutingStateIds --type KeywordList
+  tctl search-attribute  create -name IwfExecutingStateIds -type Keyword -y
   sleep 0.1
-  temporal  operator search-attribute  create --name CustomKeywordField --type Keyword
+  tctl search-attribute  create -name CustomKeywordField -type Keyword -y
   sleep 0.1
-  temporal  operator search-attribute  create --name CustomIntField --type Int
+  tctl search-attribute  create -name CustomIntField -type Int -y
   sleep 0.1
-  temporal  operator search-attribute  create --name CustomBoolField --type Bool
+  tctl search-attribute  create -name CustomBoolField -type Bool -y
   sleep 0.1
-  temporal  operator search-attribute  create --name CustomDoubleField --type Double
+  tctl search-attribute  create -name CustomDoubleField -type Double -y
   sleep 0.1
-  temporal  operator search-attribute  create --name CustomDatetimeField --type Datetime
+  tctl search-attribute  create -name CustomDatetimeField -type Datetime -y
   sleep 0.1
-  temporal  operator search-attribute  create --name CustomStringField --type Text
-  sleep 0.1
+  tctl search-attribute  create -name CustomStringField -type text -y
 
-  if checkExists "IwfWorkflowType" && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" && checkExists "CustomKeywordField" && checkExists "CustomIntField" && checkExists "CustomBoolField" && checkExists "CustomDoubleField" && checkExists "CustomDatetimeField" && checkExists "CustomStringField"; then
+  if checkExists "IwfWorkflowType" ] && checkExists "IwfGlobalWorkflowVersion" && checkExists "IwfExecutingStateIds" && checkExists "CustomKeywordField" && checkExists "CustomIntField" && checkExists "CustomBoolField" && checkExists "CustomDoubleField" && checkExists "CustomDatetimeField" && checkExists "CustomStringField" ] ; then
     echo "All search attributes are registered"
     break
   fi
 
 done
 
+tctl namespace register default
 
 tail -f /dev/null
