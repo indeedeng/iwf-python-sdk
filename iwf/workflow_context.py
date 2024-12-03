4e8ba15bd9eb52f019521cc86be18958ca8db343
lwolczynski <lwolczynski7@gmail.com>
2024-12-03 10:36:57 -0600

    IWF-356: Fix child_workflow_request_id

--- iwf/workflow_context.py
+++ iwf/workflow_context.py
@@ -17,16 +17,20 @@ class WorkflowContext:
 
 
 def _from_idl_context(idl_context: Context) -> WorkflowContext:
+    state_execution_id = unset_to_none(idl_context.state_execution_id)
+
     return WorkflowContext(
         workflow_id=idl_context.workflow_id,
         workflow_run_id=idl_context.workflow_run_id,
         workflow_start_timestamp_seconds=idl_context.workflow_started_timestamp,
-        state_execution_id=unset_to_none(idl_context.state_execution_id),
+        state_execution_id=state_execution_id,
         first_attempt_timestamp_seconds=unset_to_none(
             idl_context.first_attempt_timestamp,
         ),
         attempt=unset_to_none(idl_context.attempt),
-        child_workflow_request_id=unset_to_none(idl_context.workflow_run_id)
-        + "-"
-        + unset_to_none(idl_context.state_execution_id),
+        child_workflow_request_id=(
+            idl_context.workflow_run_id + "-" + state_execution_id
+            if state_execution_id is not None
+            else None
+        ),
     )
