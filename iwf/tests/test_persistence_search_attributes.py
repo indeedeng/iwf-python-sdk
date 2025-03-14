34520db3ddd15fd56540a4e4dc3243f447ed489f
lwolczynski <lwolczynski7@gmail.com>
2025-03-14 13:08:23 -0500

    IWF-628: Simplify test

--- iwf/tests/test_persistence_search_attributes.py
+++ iwf/tests/test_persistence_search_attributes.py
@@ -3,10 +3,9 @@ import time
 import unittest
 
 from iwf.client import Client
-from iwf.command_request import CommandRequest, TimerCommand
+from iwf.command_request import CommandRequest
 from iwf.command_results import CommandResults
 from iwf.communication import Communication
-from iwf.communication_schema import CommunicationSchema, CommunicationMethod
 from iwf.iwf_api.models import SearchAttributeValueType
 from iwf.persistence import Persistence
 from iwf.persistence_schema import PersistenceField, PersistenceSchema
@@ -16,7 +15,6 @@ from iwf.tests.worker_server import registry
 from iwf.workflow import ObjectWorkflow
 from iwf.workflow_context import WorkflowContext
 from iwf.workflow_state import T, WorkflowState
-from iwf.rpc import rpc
 
 test_internal_channel = "test-1"
 
@@ -28,12 +26,12 @@ sa_bool_key = "CustomBoolField"
 sa_datetime_key = "CustomDatetimeField"
 sa_keyword_array_key = "CustomKeywordArrayField"
 
-sa_keyword: str = "keyword"
-sa_text: str = "text"
-sa_double: float = 2.34
-sa_int: int = 234
-sa_datetime: str = "2024-11-12T16:00:01.731455544-08:00"
-sa_keyword_array: list[str] = ["keyword-1", "keyword-2"]
+# sa_keyword: str = "keyword"
+# sa_text: str = "text"
+# sa_double: float = 2.34
+# sa_int: int = 234
+# sa_datetime: str = "2024-11-12T16:00:01.731455544-08:00"
+# sa_keyword_array: list[str] = ["keyword-1", "keyword-2"]
 
 final_sa_keyword: str = "final_keyword"
 final_sa_text = None
@@ -51,9 +49,7 @@ class SearchAttributeState(WorkflowState[None]):
         persistence: Persistence,
         communication: Communication,
     ) -> CommandRequest:
-        return CommandRequest.for_all_command_completed(
-            TimerCommand.by_seconds(10),
-        )
+        return CommandRequest.empty()
 
     def execute(
         self,
@@ -75,11 +71,6 @@ class SearchAttributeState(WorkflowState[None]):
 
 
 class PersistenceSearchAttributesWorkflow(ObjectWorkflow):
-    def get_communication_schema(self) -> CommunicationSchema:
-        return CommunicationSchema.create(
-            CommunicationMethod.internal_channel_def(test_internal_channel, int),
-        )
-
     def get_workflow_states(self) -> StateSchema:
         return StateSchema.with_starting_state(SearchAttributeState())
 
@@ -108,19 +99,6 @@ class PersistenceSearchAttributesWorkflow(ObjectWorkflow):
             ),
         )
 
-    @rpc()
-    def test_persistence_set_search_attribute(self, persistence: Persistence):
-        return (
-            persistence.set_search_attribute_keyword(sa_keyword_key, sa_keyword),
-            persistence.set_search_attribute_text(sa_text_key, sa_text),
-            persistence.set_search_attribute_double(sa_double_key, sa_double),
-            persistence.set_search_attribute_int64(sa_int_key, sa_int),
-            persistence.set_search_attribute_datetime(sa_datetime_key, sa_datetime),
-            persistence.set_search_attribute_keyword_array(
-                sa_keyword_array_key, sa_keyword_array
-            ),
-        )
-
 
 class TestPersistenceSearchAttributes(unittest.TestCase):
     @classmethod
@@ -136,32 +114,26 @@ class TestPersistenceSearchAttributes(unittest.TestCase):
             PersistenceSearchAttributesWorkflow, wf_id, 100, None
         )
 
-        self.client.invoke_rpc(
-            wf_id,
-            PersistenceSearchAttributesWorkflow.test_persistence_set_search_attribute,
-        )
-
-        # Wait for the search attributes to be set; long sleep to avoid test flakiness
-        # TODO: Should be replaced with setting within a state and wait_for_state_execution_completed once implemented
-        # https://github.com/indeedeng/iwf-python-sdk/issues/48
-        time.sleep(10)
-
-        returned_search_attributes = self.client.get_workflow_search_attributes(
-            PersistenceSearchAttributesWorkflow, wf_id
-        )
-
-        expected_search_attributes = dict()
-        expected_search_attributes[sa_keyword_key] = sa_keyword
-        expected_search_attributes[sa_text_key] = sa_text
-        expected_search_attributes[sa_double_key] = sa_double
-        expected_search_attributes[sa_int_key] = sa_int
-        expected_search_attributes[sa_keyword_array_key] = sa_keyword_array
-        expected_search_attributes[sa_datetime_key] = (
-            "2024-11-13T00:00:01.731455544Z"  # This is a bug. The iwf-server always returns utc time. See https://github.com/indeedeng/iwf/issues/261
-            # "2024-11-12T18:00:01.731455544-06:00"
-        )
-
-        assert expected_search_attributes == returned_search_attributes
+        # # Wait for the search attributes to be set; long sleep to avoid test flakiness
+        # # TODO: Should be replaced with setting within a state and wait_for_state_execution_completed once implemented
+        # # https://github.com/indeedeng/iwf-python-sdk/issues/48
+        # time.sleep(10)
+        #
+        # returned_search_attributes = self.client.get_workflow_search_attributes(
+        #     PersistenceSearchAttributesWorkflow, wf_id
+        # )
+        #
+        # expected_search_attributes = dict()
+        # expected_search_attributes[sa_keyword_key] = sa_keyword
+        # expected_search_attributes[sa_text_key] = sa_text
+        # expected_search_attributes[sa_double_key] = sa_double
+        # expected_search_attributes[sa_int_key] = sa_int
+        # expected_search_attributes[sa_keyword_array_key] = sa_keyword_array
+        # expected_search_attributes[sa_datetime_key] = (
+        #     "2024-11-13T00:00:01.731455544Z"  # This is a bug. The iwf-server always returns utc time. See https://github.com/indeedeng/iwf/issues/261
+        # )
+        #
+        # assert expected_search_attributes == returned_search_attributes
 
         self.client.wait_for_workflow_completion(wf_id, None)
 
@@ -172,7 +144,7 @@ class TestPersistenceSearchAttributes(unittest.TestCase):
         final_expected_search_attributes = dict()
         final_expected_search_attributes[sa_keyword_key] = final_sa_keyword
         final_expected_search_attributes[sa_text_key] = ""
-        final_expected_search_attributes[sa_double_key] = sa_double
+        # final_expected_search_attributes[sa_double_key] = sa_double
         final_expected_search_attributes[sa_int_key] = final_sa_int
         final_expected_search_attributes[sa_bool_key] = final_sa_bool
         final_expected_search_attributes[sa_keyword_array_key] = final_sa_keyword_array
