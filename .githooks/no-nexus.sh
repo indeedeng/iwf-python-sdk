43842162f82a6b902d47e72508798316d0e7aeeb
Samuel Caçador <20863811+samuel27m@users.noreply.github.com>
2025-06-13 18:41:24 +0100

    add pre-commit hook and no-nexus script

---
+++ .githooks/no-nexus.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+sed -i'' \
+    -e '/./{H;$!d}' \
+    -e 'x' \
+    -e 's|\[package.source\]\ntype\s*=\s*\"legacy\"\nurl\s*=\s*\"https://nexus.corp.indeed.com/repository/pypi/simple\"\nreference\s*=\s*\"nexus\"||' \
+    poetry.lock
+
+sed -i'' \
+    -e '1{/^\s*$/d}' \
+    poetry.lock
+
+sed -i'' \
+    -e '/^\s*$/N;/^\s*\n$/D' \
+    poetry.lock
+
+CHANGES=$(git diff --exit-code poetry.lock | grep -Pzo '\-\[package.source\]\n\-type = "legacy"\n\-url = "https://nexus.corp.indeed.com/repository/pypi/simple"\n\-reference = "nexus"\n' | wc -c)
+
+if [[ $CHANGES -eq 0 ]]; then
+  exit 0
+fi
+
+exit 1
